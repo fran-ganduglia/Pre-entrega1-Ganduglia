a4aae4dbc9a8d3ed5e78e67f2e39c5d453498a7c
Francisco Ganduglia <franciscoganduglia@gmail.com>
2023-07-19 16:49:33 -0300

    Primer commit para pre-entrega1

---
+++ main.js
@@ -0,0 +1,69 @@
+
+const valorProducto = (productoElegido) => {
+switch (productoElegido){
+    case 1:
+    if (productoElegido = 1){
+    return precio1
+    }
+    break;
+
+    case 2:
+    if (productoElegido = 2){
+    return precio2
+    }
+    break;
+
+    case 3:
+    if (productoElegido = 3){
+    return precio3
+    }
+    break;
+
+    default:
+    if (productoElegido != 1, 2, 5){
+}
+}
+}
+
+const precio1 = 1000;
+const precio2 = 2000;
+const precio3 = 5000;
+
+let productoElegido = parseInt (prompt("Elija su producto entre el 1, 2 o el 3"));
+
+
+let cuotas = parseInt(prompt("En cuántas cuotas 3, 6 o 12?"));
+
+
+
+const calculadorCuotas = (cuotas, valorProductos) => {
+
+    switch (valorFinal) {
+        case value1:
+            if (cuotas = 3){
+            return parseFloat (valorProductos / 3);
+            }
+            break;
+
+        case value2:
+            if (cuotas = 6){
+            return parseFloat(valorProductos / 6); 
+            }
+            break;
+
+        case value3:
+            if (cuotas = 12){
+            return valorProductos / 12;
+            }
+            break;
+    
+        default:
+            break;
+    }
+} 
+
+
+let valorProductos = valorProducto = (productoElegido);
+
+let resultado = calculadorCuotas = (cuotas, valorProductos);
+alert (`Su resultado es ${resultado}`);
