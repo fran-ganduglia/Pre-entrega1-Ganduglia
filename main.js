e1f3e297391bd79a73e2268cb4231c6c9a2c6a16
Francisco Ganduglia <franciscoganduglia@gmail.com>
2023-08-10 17:58:46 -0300

    Entrega 2

--- main.js
+++ main.js
@@ -3,75 +3,65 @@ const calculadorCuotas = (a, b) => {
             return parseFloat(a / b);
     }
 
-const precio1 = 1000
-const precio2 = 2000
-const precio3 = 5000
-let productoElegido = prompt("Elija un número entre 1, 2 o 3")
+let productos = [
+        { id: 1, nombre: "PAD", precio: 1000, stock: 10, },
+        { id: 2, nombre: "MOUSE", precio: 2000, stock: 6, },
+        { id: 3, nombre: "TECLADO", precio: 5000, stock: 0, },
+        { id: 4, nombre: "AURICULARES", precio: 7000, stock: 0, },
+    ];
 
-switch (productoElegido){
-    case "1":
-        if (productoElegido = 1){
-            productoElegido = 1000
-            alert(`Su valor elegido tiene de precio ${precio1}`);
-        }
-        break;
+let productoElegido = prompt("Elija un producto de nuestra página").toUpperCase();
 
-    case "2":
-        if (productoElegido = 2){
-            productoElegido = 2000
-            alert(`Su valor elegido tiene de precio ${precio2}`);
-        }
-        break;
+const producto = productos.find((item) => item.nombre === productoElegido);
 
-    case "3":
-        if (productoElegido = 3){
-            productoElegido = 5000
-            alert(`Su valor elegido tiene de precio ${precio3}`);
-        }
-        break;
-
-    default:
-        if (productoElegido != 1, 2, 3){
-            alert("Ingrese un numero válido")
-            productoElegido = prompt("Elija un numero")
-        }
-        break;
+if (producto){
+    alert(`Su producto tiene un valor de ${producto.precio}`)
+}
+else{
+    alert("Elija un producto válido");
 }
 
-let cuotas = parseInt(prompt("Elija cuantas cuotas"));
-let resultado = calculadorCuotas (`${productoElegido}`, `${cuotas}`);
+let cuotas = parseInt(prompt("Elija cantidad de cuotas"));
+let resultado = calculadorCuotas (`${producto.precio}`, `${cuotas}`);
 
-    switch(cuotas){
-        case 3:
-            if(cuotas = 3){
-                alert (`Su resultado es ${resultado}`);
-            }
-            break;
-        case 6:
-            if(cuotas = 6){
-                alert (`Su resultado es ${resultado}`);
-            }
-            break;
-        case 12:
-            if(cuotas = 12){
-                alert (`Su resultado es ${resultado}`);
-            }
-            break;
-        default:
-        alert("Elija un número entre 3, 6 y 12 respectivo a las cuotas");
-        break;
-  }
+     switch(cuotas){
+         case 3:
+             if(cuotas = 3){
+                 alert (`Su resultado es ${resultado}`);
+             }
+             break;
+         case 6:
+             if(cuotas = 6){
+                 alert (`Su resultado es ${resultado}`);
+             }
+             break;
+         case 12:
+             if(cuotas = 12){
+                 alert (`Su resultado es ${resultado}`);
+             }
+             break;
+         default:
+             while (cuotas !== 3 && 6 && 12) {
+                 alert("Elija un número entre 3, 6 y 12 respectivo a las cuotas");
+                 cuotas = parseInt(prompt("Elija cuantas cuotas"));
+             }
+             break
+   }
 
-let compraSeguro = prompt("Esta seguro de su compra?").toUpperCase();
+ let compraSeguro = prompt("Esta seguro de su compra?").toUpperCase();
 
-while (compraSeguro !== "SI"){
-    if (compraSeguro === "NO"){
-        alert("Escriba SI y refresque la página para volver a empezar")
-    }
-    else if (compraSeguro !== "SI"){
-        alert("Ingrese SI o NO");
-    } 
-    compraSeguro = prompt("Esta seguro de su compra?").toUpperCase();
-}
+ while (compraSeguro !== "NO" || "SI"){
+     if (compraSeguro === "NO"){
+         alert("Puede volver a empezar")
+         break
+     }
+     else if (compraSeguro !== "SI" && "NO"){
+         alert("Ingrese SI o NO");
+     } 
+     else if (compraSeguro === "SI"){
+         alert("Su compra ha sido exitosa");
+         break
+     }
+     compraSeguro = prompt("Esta seguro de su compra?").toUpperCase();
+ }
 
-alert("Su compra ha sido exitosa")
